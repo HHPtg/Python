86dd8b478a7ee35ca3043aa9587933739a0b8628
Hanif <hanif2403@gmail.com>
2020-10-01 11:18:20 +0300

    Error fixed

---
+++ madlibs.py
@@ -0,0 +1,25 @@
+#Knowing the user's name
+user_name=input("Anata no onamae wa? ")
+print ("well hello there " + user_name +"-chan")
+
+#declartation of the basis of the mad libs
+print("this madlibs is based on a stupid story made by your beloved senseis :)" )
+
+#begining the madlibs (userinput)
+input1=input("Choose an adjective Please? ")
+input2=input("please input a verb in past tense ")
+input3=input("please input a number ")
+input4=input("please input a name ")
+input5=input("please input another name ")
+input6=input("please input a plural noun ")
+
+#begining of the user generated madlibs
+print("long ago there were 2 young children named " + input4 + " and " + input5 + " Both of them were very " + input1 + ". One day they were walking down the street when they saw a man holding " + input3 + " " + input6 + " so they " + input2 + " in pure happiness. " + " THE DUCKIN ENDDDDDD" )
+
+#credits
+print("made by Hanif and Vincent")
+
+close=input()
+close=input()
+close=input()
+close=input()
---
+++ project1.py
@@ -0,0 +1,17 @@
+item=input("please input the item being purchased: ")
+price=input("please input the price of item being purchased: ")
+print("would you like to add another item? yes/no ")
+print("maximum two items")
+while True:
+    choice = input("")
+
+    if choice in ('yes'):
+      item2=input("please input the item being purchased: ")
+      price2=input("please input the price of item being purchased: ")
+      tt = (int(price) + int(price2))
+    if choice in ('no'):
+      print("your reciept:   " + item + "       " + price + "         " + item2 + "          " + price2 + "                total")
+      print(tt)
+      print( "                      ********Thanks For Shopping*******")
+      input("")
+      input("")
