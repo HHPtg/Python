521782817ba8c49619009bf55db5c2722a093e63
Hanif Patel <hanif2403@gmail.com>
2021-01-06 19:40:07 +0300

    failed attempts to fix code

--- Quiz.py
+++ Quiz.py
@@ -1,34 +1,26 @@
-def start ():
-    print("Welcome to the quiz")
-    print("Best of Luck :) PS: you only have one life :)")
-
-#def sorry
-def sorry() :
+#def 
+def ask(question, answer):
+  result = input(question)
+  if result == answer:
+      print("ok")
+      print('1 point')
+  else:
+    print("wrong")
     print("Sorry You lose :(")
-    restart()
-#def restart
-def restart():
     resulta = input("wanna try again? y/n" )
     if resulta == "y":
         print("ok")
+        quiz()
     else:
         print("Alright, goodbye")
-
-#def 
-def ask(question, answer):
-  result = input("")
-  if result == answer:
-      print("ok")
-  else:
-    print("wrong")
-    sorry()
+        exit()
+    
 
 #Quiz
 def quiz() :
-start()
-print('what is 2+2?' )
-ask("what is 2+2","4" )
-print('what is 4+4?' )
-ask("what is 4+4","8" )
-
-
+    print("welcome to the quiz")
+    ask("what is 2+2","4" )
+    ask("what is 4+4","8" )
+    ask("what is 8+8","16" )
+    ask("what is 16+16","32" )
+quiz()
