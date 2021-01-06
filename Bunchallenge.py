4e85171dc6eaba380303ac2886587ad5ba901374
Hanif Patel <hanif2403@gmail.com>
2021-01-06 23:25:37 +0300

    completed challenge

---
+++ Bunchallenge.py
@@ -0,0 +1,10 @@
+import requests, json
+city_name = input("Enter city name : ") 
+base_url = "http://api.openweathermap.org/data/2.5/weather?q="+city_name+"&appid=d63af4c2b637eccb56f846d82d6c742f" 
+response = requests.get(base_url) 
+weather_json = response.json()
+if weather_json["cod"] != "404": 
+    y = weather_json["main"] 
+current_temperature = y["temp"]
+c = current_temperature-273.15
+print ("Temperature in °C = " + str(c)+"°" )
