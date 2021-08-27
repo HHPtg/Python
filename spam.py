368f66cf35a63de2fc4e87419bd9bef41e40118f
Hanif Patel <hanif2403@gmail.com>
2021-08-27 17:12:04 +0300

    updates?

--- spam.py
+++ spam.py
@@ -2,8 +2,12 @@ import pyautogui,time
 time.sleep(15)
 def restart():
     pyautogui.click()
+    #copy and paste last copy
     pyautogui.press('ctrl'+'v')
+    #send
     pyautogui.press('enter')
     time.sleep(0.01)
     restart()
 restart()
+time.sleep(2.00)
+quit()
