984a44d0e5f8c91d301be5394528830936c9537b
HHPtg <hanif2403@gmail.com>
2023-11-17 00:57:27 -0500

    added comment

--- Bunchallenge.py
+++ Bunchallenge.py
@@ -1,5 +1,6 @@
 #imports
 import requests, json
+#recursion
 def restart():
     city_name = input("Enter city/Country name : ") 
     base_url = "http://api.openweathermap.org/data/2.5/weather?q="+city_name+"&appid=d63af4c2b637eccb56f846d82d6c742f" 
